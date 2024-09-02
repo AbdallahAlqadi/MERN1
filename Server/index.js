5ba1513b72da37356948e2df6821b5c23f6a436d
AbdallahAlqadi <161605945+AbdallahAlqadi@users.noreply.github.com>
2024-09-02 14:47:45 +0300

    add

---
+++ Server/index.js
@@ -0,0 +1,35 @@
+// backend fromwork
+const express=require('express')
+// DB
+const mongoose=require('mongoose')
+// Routes
+const bodyparser=require('body-parser')
+// midedelware:to make the front-end able to send requst
+const cors=require('cors');
+// TO TAKE VARIBALE  FROM  THE .ENV FILE
+require('dotenv').config();
+
+// THE MAIN APP
+const app=express();
+app.use(bodyparser.json());
+
+app.use(cors());
+mongoose.connect(process.env.MONGO_URI,{
+
+
+    useNewUrlParser:true,
+    useUnfiedTopology:true
+}).then(()=>console.log("Connected to MongoDB")).catch(err=>console.log(err))
+
+app.get('/test',(req,res)=>{
+    res.send('Hello from the Server');
+})
+
+
+
+const PORT=process.env.PORT||5001;
+app.listen(PORT,()=>{
+
+    console.log(`Server Running on port ${PORT}`);
+})
+
