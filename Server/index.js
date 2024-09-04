a991ff93d78b283d82068de9c2512ea1c29a062b
AbdallahAlqadi <161605945+AbdallahAlqadi@users.noreply.github.com>
2024-09-04 11:59:08 +0300

    add

--- Server/index.js
+++ Server/index.js
@@ -18,7 +18,7 @@ mongoose.connect(process.env.MONGO_URI,{
 
 
     useNewUrlParser:true,
-    useUnfiedTopology:true
+    useUnifiedTopology: true
 }).then(()=>console.log("Connected to MongoDB")).catch(err=>console.log(err))
 
 app.get('/test',(req,res)=>{
