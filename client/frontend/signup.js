28d360c13dddc25b5e896f38a2fc5ecaabebbbd0
AbdallahAlqadi <161605945+AbdallahAlqadi@users.noreply.github.com>
2024-10-05 08:01:54 +0300

    add

--- client/frontend/signup.js
+++ client/frontend/signup.js
@@ -102,7 +102,7 @@ async function updateUser(id) {
 
     try {
         const response = await fetch(`http://127.0.0.1:5002/api/signup/update/${id}`, {
-            method: 'PUT',
+            method: 'PUT',  //لازم capital
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify(updatedUser)
         });
