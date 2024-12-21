12cea4d6193e49918800ba0aadc23765e49a378d
AbdallahAlqadi <161605945+AbdallahAlqadi@users.noreply.github.com>
2024-12-21 17:59:43 +0300

    add

--- Server/models/users.js
+++ Server/models/users.js
@@ -4,8 +4,9 @@ const mongoose=require('mongoose')
 //انه اي user بدي اضيفه لازم يحتوي على username,phone
 const userSchema=new mongoose.Schema({
 username:{type:String,required:true},
-phone:{type:Number,required:true,unique: true},
-password:{type:String,required:true,unique: true}
+phone:{type:Number,required:true},
+password:{type:String,required:true,unique: true},
+roule:{type:String,required:true,default:'user'},
 
 })
 
