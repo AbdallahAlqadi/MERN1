5758b1abd0c775b1f42a0e43728a8ef18a57a640
AbdallahAlqadi <161605945+AbdallahAlqadi@users.noreply.github.com>
2024-12-27 14:26:58 +0300

    add

--- client/frontreact/src/App.js
+++ client/frontreact/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './component/home';
 import Login from './component/loginCheck';
+import DashboardLayoutBasic  from './component/tolpad';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
           <Route path="/" element={<Login />} />
           <Route path="/home" element={<Home />} />
           <Route path="/login" element={<Login />} />
+          <Route path="/tolpad" element={<DashboardLayoutBasic />} />
 
 
         </Routes>
