15d6f2648c98971aa3de910769bf39ebec679668
AbdallahAlqadi <161605945+AbdallahAlqadi@users.noreply.github.com>
2025-01-22 20:56:10 +0300

    add

--- client/frontreact/src/component/tolpad.js
+++ client/frontreact/src/component/tolpad.js
@@ -4,6 +4,9 @@ import Typography from '@mui/material/Typography';
 import { createTheme } from '@mui/material/styles';
 import DashboardIcon from '@mui/icons-material/Dashboard';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import BarChartIcon from '@mui/icons-material/BarChart';
+import DescriptionIcon from '@mui/icons-material/Description';
+import LayersIcon from '@mui/icons-material/Layers';
 import { AppProvider } from '@toolpad/core/AppProvider';
 import { DashboardLayout } from '@toolpad/core/DashboardLayout';
 import { useNavigate } from 'react-router-dom';
@@ -11,15 +14,18 @@ import React, { useEffect, useState } from 'react';
 import ExitToAppIcon from '@mui/icons-material/ExitToApp';
 import { PageContainer } from '@toolpad/core/PageContainer';
 import PersonIcon from '@mui/icons-material/Person';
+import Newdash from './newdah';
+import Orders from './oreders';
 import axios from 'axios';
+import Users from './user';
 
+
+//موجود
 const demoTheme = createTheme({
-  palette: {
-    mode: 'light', // تعيين الوضع إلى الوضع الفاتح (الأبيض)
-    background: {
-      default: '#ffffff', // تعيين لون الخلفية إلى الأبيض
-    },
+  cssVariables: {
+    colorSchemeSelector: 'data-toolpad-color-scheme',
   },
+  colorSchemes: { light: true, dark: true },
   breakpoints: {
     values: {
       xs: 0,
@@ -33,6 +39,7 @@ const demoTheme = createTheme({
 
 function useDemoRouter(initialPath) {
   const [pathname, setPathname] = React.useState(initialPath);
+//addcode
 
   const router = React.useMemo(() => {
     return {
@@ -69,9 +76,19 @@ function DashboardLayoutBasic(props) {
   const { window } = props;
   const [user, setUser] = useState({});
   const navigate = useNavigate();
+//addcode
+  const allPages = [
+    { path: '/dashboard', component: <Newdash /> },
+    { path: '/orders', component: <Orders /> },
+    { path: '/users', component: <Users /> },
+  ];
 
+  const [currentComponent, setCurrentComponent] = useState(<Newdash />);
   const [dashNavigate, setDashNavigate] = useState([
-  
+    {
+      kind: 'header',
+      title: 'Main items',
+    },
     {
       segment: 'dashboard',
       title: 'Dashboard',
@@ -83,10 +100,12 @@ function DashboardLayoutBasic(props) {
       icon: <ShoppingCartIcon />,
     },
     {
+      kind: 'divider',
+    },
+    {
       segment: 'LogOut',
       title: 'LogOut',
       icon: <ExitToAppIcon />,
-      onClick: () => handleLogOut(), // ربط handleLogOut هنا
     },
   ]);
 
@@ -95,17 +114,16 @@ function DashboardLayoutBasic(props) {
     navigate('/');
   };
 
-
   const router = useDemoRouter('/dashboard');
 
   useEffect(() => {
     const token = sessionStorage.getItem('jwt');
-
+//addnewcode
     const invaliedToken = async () => {
       try {
-        const res = await axios.get('http://127.0.0.1:5003/api/home', {
+        const res = await axios.get('http://127.0.0.1:5002/api/home', {
           headers: {
-            Auth: 'Bearer ' + token,
+            auth: 'Bearer ' + token,
           },
         });
 
@@ -135,10 +153,39 @@ function DashboardLayoutBasic(props) {
               icon: <PersonIcon />,
             },
             {
+              kind: 'divider',
+            },
+            {
+              kind: 'header',
+              title: 'Analytics',
+            },
+            {
+              segment: 'reports',
+              title: 'Reports',
+              icon: <BarChartIcon />,
+              children: [
+                {
+                  segment: 'sales',
+                  title: 'Sales',
+                  icon: <DescriptionIcon />,
+                },
+                {
+                  segment: 'traffic',
+                  title: 'Traffic',
+                  icon: <DescriptionIcon />,
+                },
+              ],
+            },
+            {
+              segment: 'integrations',
+              title: 'Integrations',
+              icon: <LayersIcon />,
+            },
+            {
               segment: 'LogOut',
               title: 'LogOut',
+              onClick: handleLogOut,
               icon: <ExitToAppIcon />,
-              onClick: () => handleLogOut(), // ربط handleLogOut هنا أيضًا
             },
           ]);
         }
@@ -154,17 +201,21 @@ function DashboardLayoutBasic(props) {
     };
 
     invaliedToken();
+    setCurrentComponent(allPages.find((page) => page.path === router.pathname)?.component);
   }, [router]);
 
+
   const demoWindow = window !== undefined ? window() : undefined;
+
+  const deletetoken= async () => {
+    
+      }
 
   return (
     <AppProvider navigation={dashNavigate} router={router} theme={demoTheme} window={demoWindow}>
       <DashboardLayout>
         <Typography>{user.roul}</Typography>
-        <PageContainer>
-          <DemoPageContent pathname={router.pathname} />
-        </PageContainer>
+        <PageContainer>{currentComponent}</PageContainer>
       </DashboardLayout>
     </AppProvider>
   );
